274f45cf2bfe40d233023895584ae7d87fa78aaa
Inffinite <louislaizer4@gmail.com>
2022-02-15 13:49:22 +0300

    :hammer: Contract for wife to send money to herslf
    And husband to mint and send to whoever

--- contracts/Wife.sol
+++ contracts/Wife.sol
@@ -2,17 +2,48 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Wife {
-  uint public wifeMoney;
+  address public minter;
+  mapping (address => uint) public balances;
+  address public wifesAddress;
 
+  // Events allow clients to react to specific
+  // contract changes in this case the sending of
+  // money to my wife
+  event Sent(address from, address to, uint amount);
+
+  // runs once when the contract 
+  // is created only
   constructor(){
-    wifeMoney = 23;
+    // set the address
+    // of the one who uploaded contract as the minter
+    // in this case the husband
+    minter = msg.sender;
+    // set wife's address
+    wifesAddress = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
   }
 
-  function set(uint x) public {
-    wifeMoney = x;
+  // can only be called by the minter(husband)
+  // mint and send to any address
+
+  function mint(address receiver, uint amount) public {
+    require(msg.sender == minter);
+    balances[receiver] += amount;
   }
 
-  function get() public view returns (uint){
-    return wifeMoney;
+  error InsufficientBalance(uint requested, uint available);
+  
+  // can be used by anyone to send money
+  // can only send money to the wife
+  function sendToWife(address receiver, uint amount) public {
+    require(receiver == wifesAddress);
+    if(amount > balances[msg.sender])
+      revert InsufficientBalance({
+        requested: amount,
+        available: balances[msg.sender]
+      });
+
+    balances[msg.sender] -= amount;
+    balances[receiver] += amount;
+    emit Sent(msg.sender, receiver, amount);
   }
 }
