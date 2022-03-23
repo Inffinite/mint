e7a02556c19ff396b4bc8a922e595ee1f5887a0c
Lewis <louislaizer4@gmail.com>
2022-03-23 12:18:21 +0300

    Nuked

--- contracts/YatchAuction.sol
+++ contracts/YatchAuction.sol
@@ -1,12 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-// Contract for auction to sell my yatch
-// Open Auction
-
-// Beneficiary is the yatch owner who will receive all the money
-// once the bid is over
-
 contract Yatch{
     address payable public beneficiary;
     uint public auctionEndTime;
@@ -14,27 +8,19 @@ contract Yatch{
     address public highestBidder;
     uint public highestBid;
 
-    // Allowed withdrawals of previous bids
     mapping(address => uint) pendingReturns;
 
-    // Set to true at the end, disallows any change.
-    // By default initialized to `false`.
     bool ended;
 
-    // change events
 
     event HighestBidIncreased(address bidder, uint amount);
     event AuctionEnded(address winner, uint amount);
-
-    // errors that describe failures
 
     error AuctionAlreadyEnded();
     error BidNotHighEnough(uint highestBid);
     error AuctionNotYetEnded();
     error AuctionEndAlreadyCalled();
 
-    // bidding time is number of seconds until 
-    // auction ends
     constructor(
         uint biddingTime,
         address payable beneficiaryAddress
@@ -43,10 +29,6 @@ contract Yatch{
         auctionEndTime = block.timestamp + biddingTime;
     }
 
-    /// Bid on the auction with the value sent
-    /// together with this transaction.
-    /// The value will only be refunded if the
-    /// auction is not won.
     function bid() external payable {
         if(block.timestamp > auctionEndTime)
             revert AuctionAlreadyEnded();
@@ -55,11 +37,6 @@ contract Yatch{
         revert BidNotHighEnough(highestBid);
 
     if(highestBid != 0){
-        // Sending back the money by simply using
-        // highestBidder.send(highestBid) is a security risk
-        // because it could execute an untrusted contract.
-        // It is always safer to let the recipients
-        // withdraw their money themselves.
         pendingReturns[highestBidder] += highestBid;
     }
 
@@ -72,13 +49,9 @@ contract Yatch{
     function withdraw() external returns (bool){
         uint amount = pendingReturns[msg.sender];
         if(amount > 0){
-            // It is important to set this to zero because the recipient
-            // can call this function again as part of the receiving call
-            // before `send` returns.
             pendingReturns[msg.sender] = 0;
 
             if(!payable(msg.sender).send(amount)){
-                // No need to call throw here, just reset the amount owing
                 pendingReturns[msg.sender] = amount;
                 return false;
             }
@@ -86,21 +59,7 @@ contract Yatch{
         return true;
     }
 
-    /// End the auction and send the highest bid
-    /// to the beneficiary.
     function auctionEnd() external {
-        // It is a good guideline to structure functions that interact
-        // with other contracts (i.e. they call functions or send Ether)
-        // into three phases:
-        // 1. checking conditions
-        // 2. performing actions (potentially changing conditions)
-        // 3. interacting with other contracts
-        // If these phases are mixed up, the other contract could call
-        // back into the current contract and modify the state or cause
-        // effects (ether payout) to be performed multiple times.
-        // If functions called internally include interaction with external
-        // contracts, they also have to be considered interaction with
-        // external contracts.
 
         if(block.timestamp < auctionEndTime)
             revert AuctionNotYetEnded();
